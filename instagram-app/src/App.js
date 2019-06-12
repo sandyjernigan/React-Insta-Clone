0f6539a9d906a74d87167c65f9c18170ea233be3
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-12 19:44:27 -0400

    iterate over said data, passing each individual object as a prop to an instance of PostContainer
    iterate over said data, passing each individual object as a prop to an instance of PostContainer

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { dummyData } from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
 import './App.css';
+
+// iterate over said data, passing each individual object as a prop to an instance of PostContainer
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      data: dummyData
+    };
   }
 
   render() {
     return (
       <div className="App">
+        {data.map( post => {
+            return (
+            <PostContainer post={post} />
+        );})}
       </div>
     );
   }
