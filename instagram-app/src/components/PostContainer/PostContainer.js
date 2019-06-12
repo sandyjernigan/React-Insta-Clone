c51b83548975459724069feae203278e7aec35f4
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-12 19:51:27 -0400

    Update PostContainer.js

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-// Each PostContainer component will then pass the array of comments 
-// on each post object as a prop to an instance of the CommentSection component
+// Each PostContainer component will then pass the array of comments on each post object as a prop to an instance of the CommentSection component
 
 function PostContainer(props) {
     return (
         <div className="PostContainer">
+            <CommentSection comments={props.comments} />
         </div>
     );
 }
