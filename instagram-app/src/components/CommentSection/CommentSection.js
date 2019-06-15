51b94fda2c20647ef60314609c740f35846c299c
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-15 12:53:01 -0400

    changing CommentSection to a class with state

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -8,16 +8,29 @@ import './Comments.css';
 // and render a Comment component with the username of the poster as well as the post's text. 
 // Additionally, there should be an input box that allows users to submit a new comment for any post. 
 
-function CommentSection(props) {
-    return (
-        <div className="CommentSection">
-            {props.comments.map( (each, i) => {
-                return (
-                    <Comment comment={each} key={i} />
-            );})}
-        <CommentInput />
-        </div>
-    );
+// Lets divide up the data that we've been working with this far by separating the comments array onto a new component's state. 
+// Pass down the comments through each post to the CommentSection component. 
+// Be sure that you set the incoming comments props data on the state of the CommentSection component.
+
+class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: props.comments
+        };
+    }
+
+    render() {
+        return (
+            <div className="CommentSection">
+                {this.state.comments.map( (each, i) => {
+                    return (
+                        <Comment comment={each} key={i} />
+                );})}
+            <CommentInput />
+            </div>
+        );
+    }
 }
 
 // Be sure to check the Types of the data you are passing around as props in the components 
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -5,6 +5,9 @@ import PostTimeStamp from './PostTimeStamp';
 import CommentSection from '../CommentSection/CommentSection'
 import './Post.css';
 
+// Each PostContainer component will then pass the array of comments on each post object 
+// as a prop to an instance of the CommentSection component.
+
 function PostContainer(props) {
     return (
         <div className="PostContainer">
@@ -33,6 +36,3 @@ PostContainer.propTypes = {
 };
 
 export default PostContainer;
-
-// Each PostContainer component will then pass the array of comments on each post object 
-// as a prop to an instance of the CommentSection component.
